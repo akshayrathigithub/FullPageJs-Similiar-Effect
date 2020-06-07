6d0c5a2855355014018e6d0b6be5c519c1594e8b
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-07 21:57:45 +0530

    arrow addition

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const BtnUp = document.getElementById('BtnUp')
-const BtnDown = document.getElementById('BtnDown')
+const BtnUp = document.getElementById('Up')
+const BtnDown = document.getElementById('Down')
 const PageWrapper = document.getElementById('PageWrapper')
-let ind = 1
+let ind = 0
 BtnUp.addEventListener('click', ()=>{
+    if(ind === 0 || ind > 6){
+        ind = 5
+    }else{
+        ind = ind-1
+    }
     PageWrapper.style.transform = `translate3d(0px, -${ind*(100)}%, 0px)`
-    ind = ind+1
     console.log(ind)
 })
 BtnDown.addEventListener('click', ()=>{
-    ind = ind-1
+    if(ind === 5){
+        ind = 0
+    }else{
+        ind = ind+1
+    }
     PageWrapper.style.transform = `translate3d(0px, -${ind*(100)}%, 0px)`
     console.log(ind)
 })
