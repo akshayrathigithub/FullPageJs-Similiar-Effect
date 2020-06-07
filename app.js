2f841da842b3c55f75aa9e3a63d15493af55a61c
Akshay Rathi <akshayrathi96@gmail.com>
2020-06-07 15:52:31 +0530

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello ");
+const BtnUp = document.getElementById('BtnUp')
+const BtnDown = document.getElementById('BtnDown')
+const PageWrapper = document.getElementById('PageWrapper')
+let ind = 1
+BtnUp.addEventListener('click', ()=>{
+    PageWrapper.style.transform = `translate3d(0px, -${ind*(100)}%, 0px)`
+    ind = ind+1
+    console.log(ind)
+})
+BtnDown.addEventListener('click', ()=>{
+    ind = ind-1
+    PageWrapper.style.transform = `translate3d(0px, -${ind*(100)}%, 0px)`
+    console.log(ind)
+})
